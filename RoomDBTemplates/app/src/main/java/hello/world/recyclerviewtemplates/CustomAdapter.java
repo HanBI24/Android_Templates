0319eb726d957aacb238d7543d16d7147739fca1
HanBI24 <alsltjdrhs@naver.com>
2020-12-31 17:28:26 +0900

    Copy RecyclerViewTemplates

---
+++ RoomDBTemplates/app/src/main/java/hello/world/recyclerviewtemplates/CustomAdapter.java
@@ -0,0 +1,113 @@
+package hello.world.recyclerviewtemplates;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.ActionMode;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+// RecyclerView CRUD.
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+    private final ArrayList<Dictionary> mList;
+    ItemClickListener itemClickListener;
+
+    // Save view by ViewHolder class.
+    public static class CustomViewHolder extends RecyclerView.ViewHolder {
+        protected TextView id;
+        protected TextView english;
+        protected TextView korean;
+        protected TextView deleteItem;
+
+        // (2)
+        // RecyclerView need to make ViewHolder to yourself.
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.id = (TextView) itemView.findViewById(R.id.id_listitem);
+            this.english= (TextView) itemView.findViewById(R.id.english_listitem);
+            this.korean = (TextView) itemView.findViewById(R.id.korean_listitem);
+            this.deleteItem = (TextView) itemView.findViewById(R.id.delete_item);
+        }
+
+    }
+
+    // (3) => Call this constructor in MainActivity.
+    // Constructor's parameter get List(ex. ArrayList) in MainActivity.
+    public CustomAdapter(ArrayList<Dictionary> list){
+        this.mList = list;
+    }
+
+    // (1) => Callback method. Call at view != null
+    // Create ViewHolder object.
+    // C, U, D된 view(리스트) 반환. (Read)
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_list, viewGroup, false);
+
+        return new CustomViewHolder(view);
+    }
+
+    // (4)
+    // Display ViewHolder data(position) at view.
+    // 리스트 추가 및 ViewHolder 구현. (Create)
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder viewHolder, int position) {
+        viewHolder.id.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26);
+        viewHolder.english.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26);
+        viewHolder.korean.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26);
+
+        viewHolder.id.setGravity(Gravity.CENTER);
+        viewHolder.english.setGravity(Gravity.CENTER);
+        viewHolder.korean.setGravity(Gravity.CENTER);
+
+        viewHolder.id.setText(mList.get(position).getId());
+        viewHolder.english.setText(mList.get(position).getEnglish());
+        viewHolder.korean.setText(mList.get(position).getKorean());
+
+        // 리스트 수정. (Update)
+        final Dictionary dictionary = mList.get(position);
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemClickListener.OnItemClick(position, dictionary);
+            }
+        });
+
+        // 리스트 삭제. (Delete)
+        viewHolder.deleteItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // mList.remove(position); => 제대로 동작하지 않음
+                mList.remove(viewHolder.getAdapterPosition());
+                notifyDataSetChanged();
+            }
+        });
+    }
+
+    public void setOnItemClickListener(ItemClickListener itemClickListener){
+        this.itemClickListener = itemClickListener;
+    }
+
+
+    // Return all item count.
+    @Override
+    public int getItemCount() {
+        return (null != mList ? mList.size() : 0);
+    }
+}
---
+++ RoomDBTemplates/app/src/main/java/hello/world/recyclerviewtemplates/MainActivity.java
@@ -0,0 +1,115 @@
+package hello.world.recyclerviewtemplates;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<Dictionary> mArrayList;
+    private CustomAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_main_list);
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        // Set LinearLayout horizontal
+        //mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        mArrayList = new ArrayList<>();
+        mAdapter = new CustomAdapter(mArrayList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // 구분선
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
+                mLinearLayoutManager.getOrientation());
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+
+        Button btnInsert = (Button)findViewById(R.id.button_main_insert);
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                View view = LayoutInflater.from(MainActivity.this)
+                        .inflate(R.layout.edit_box, null, false);
+                builder.setView(view);
+
+                final Button ButtonSubmit = (Button) view.findViewById(R.id.button_dialog_submit);
+                final EditText editTextID = (EditText) view.findViewById(R.id.edittext_dialog_id);
+                final EditText editTextEnglish = (EditText) view.findViewById(R.id.edittext_dialog_endlish);
+                final EditText editTextKorean = (EditText) view.findViewById(R.id.edittext_dialog_korean);
+                final AlertDialog dialog = builder.create();
+                ButtonSubmit.setText("삽입");
+
+                ButtonSubmit.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        String strID = editTextID.getText().toString();
+                        String strEnglish = editTextEnglish.getText().toString();
+                        String strKorean = editTextKorean.getText().toString();
+
+                        Dictionary dict = new Dictionary(strID, strEnglish, strKorean );
+                        mArrayList.add(0, dict); //첫번째 줄에 삽입됨
+                        //mArrayList.add(dict); //마지막 줄에 삽입됨
+                        // notifyItemInserted(): 특정 position에 삽입하고 싶을 때 사용
+                        mAdapter.notifyItemInserted(0);
+                        //mAdapter.notifyDataSetChanged();
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+        // 1. 인터페이스로 구현
+        // 2. CustomAdapter에 인터페이스의 Callback 메서드 Overriding 구현
+        // 3. MainActivity에서 mAdapter 객체 생성 후 Callback method Overriding
+        mAdapter.setOnItemClickListener(new ItemClickListener() {
+            @Override
+            public void OnItemClick(int position, Dictionary dictionary) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                View view = LayoutInflater.from(MainActivity.this)
+                        .inflate(R.layout.edit_box, null, false);
+                builder.setTitle("Update User Info")
+                       .setView(view);
+
+                Button btnReplace = (Button) view.findViewById(R.id.button_dialog_submit);
+                btnReplace.setText("수정");
+
+                final EditText editTextID = (EditText) view.findViewById(R.id.edittext_dialog_id);
+                final EditText editTextEnglish = (EditText) view.findViewById(R.id.edittext_dialog_endlish);
+                final EditText editTextKorean = (EditText) view.findViewById(R.id.edittext_dialog_korean);
+
+                final AlertDialog dialog = builder.create();
+                dialog.show();
+
+                btnReplace.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String strID = editTextID.getText().toString();
+                        String strEnglish = editTextEnglish.getText().toString();
+                        String strKorean = editTextKorean.getText().toString();
+                        Dictionary dict = new Dictionary(strID, strEnglish, strKorean );
+                        mArrayList.set(position, dict);
+                        mAdapter.notifyItemChanged(position);
+                        dialog.dismiss();
+                    }
+                });
+
+            }
+        });
+    }
+}
